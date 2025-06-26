e82f1bf5252d4346ce43e60d2b3b90138d3b5396
aizadramli90 <muhammadaizadr@gmail.com>
2025-06-27 05:18:57 +0800

    add payment method by importing into footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,6 +2,7 @@ import { APP_NAME } from "@/lib/constants";
 import Link from "next/link";
 import Image from "next/image";
 import registeredLogistics from "@/lib/constants/registeredLogistics";
+import paymentMethods from "@/lib/constants/paymentMethods";
 
 const Footer = () => {
   const currebtYear = new Date().getFullYear();
@@ -20,106 +21,32 @@ const Footer = () => {
 
       <div className="payment-methods flex flex-col gap-2 items-baseline">
         <h2 className="font-bold">Payments</h2>
-        <div>
-          <Image
-            src="/images/payment/visa.jpeg"
-            alt="visa"
-            width={52}
-            height={22}
-          />
-        </div>
-        <div>
-          <Image
-            src="/images/payment/mastercard.jpeg"
-            alt="mastercard"
-            width={52}
-            height={22}
-          />
-        </div>
-        <div>
-          <Image
-            src="/images/payment/googlepay.png"
-            alt="google pay"
-            width={52}
-            height={22}
-          />
-        </div>
-        <div>
-          <Image
-            src="/images/payment/fpx.jpeg"
-            alt="fpx"
-            width={52}
-            height={22}
-          />
-        </div>
-        <div>
-          <Image
-            src="/images/payment/duitnow.jpeg"
-            alt="duitnow"
-            width={52}
-            height={22}
-          />
-        </div>
-        <div>
-          <Image
-            src="/images/payment/applepay.png"
-            alt="apple pay"
-            width={52}
-            height={22}
-          />
-        </div>
+        {paymentMethods.map((payment) => {
+          return (
+            <div key={payment.id}>
+              <Image
+                src={payment.src}
+                alt="payment method"
+                width={52}
+                height={22}
+              />
+            </div>
+          );
+        })}
       </div>
 
       <div className=" flex flex-col gap-2 items-baseline">
         <h2 className="font-bold">Logistics Partner</h2>
-        <div>
-          <Image
-            src="/images/logistics/posmalaysia.png"
-            alt="pos malaysia"
-            width={52}
-            height={22}
-          />
-        </div>
-        <div>
-          <Image
-            src="/images/logistics/ninjavan.jpeg"
-            alt="ninja van"
-            width={52}
-            height={22}
-          />
-        </div>
-        <div>
-          <Image
-            src="/images/logistics/j&t.jpeg"
-            alt="J&T"
-            width={52}
-            height={22}
-          />
-        </div>
-        <div>
-          <Image
-            src="/images/logistics/gdex.png"
-            alt="gdex"
-            width={52}
-            height={22}
-          />
-        </div>
-        <div>
-          <Image
-            src="/images/logistics/flash.png"
-            alt="flash"
-            width={52}
-            height={22}
-          />
-        </div>
-        <div>
-          <Image
-            src="/images/logistics/dhl.jpeg"
-            alt="DHL"
-            width={52}
-            height={22}
-          />
-        </div>
+        {registeredLogistics.map((logistic) => (
+          <div key={logistic.id}>
+            <Image
+              src={logistic.src}
+              alt={logistic.name}
+              width={52}
+              height={22}
+            />
+          </div>
+        ))}
       </div>
 
       <div className="flex flex-col gap-1 items-start">
---
+++ lib/constants/paymentMethods.ts
@@ -0,0 +1,34 @@
+const paymentMethods = [
+  {
+    id: 1,
+    name: "visa",
+    src: "/images/payment/visa.jpeg",
+  },
+  {
+    id: 2,
+    name: "mastercard",
+    src: "/images/payment/mastercard.jpeg",
+  },
+  {
+    id: 3,
+    name: "googlepay",
+    src: "/images/payment/googlepay.png",
+  },
+  {
+    id: 4,
+    name: "fpx",
+    src: "/images/payment/fpx.jpeg",
+  },
+  {
+    id: 5,
+    name: "duitnow",
+    src: "/images/payment/duitnow.jpeg",
+  },
+  {
+    id: 6,
+    name: "applepay",
+    src: "/images/payment/applepay.png",
+  },
+];
+
+export default paymentMethods;
